0fa8ae8ef56a1a7e283ccada03a24eaf6d3954f2
seand4 <vaseant.davis@gmail.com>
2022-04-23 15:19:07 -0400

    Added Fixed header with background chang with scroll

--- public/main.js
+++ public/main.js
@@ -1,2 +1,18 @@
-console.log('this is main.js !!!!')
+var header = document.querySelector("header")
+
+window.addEventListener("scroll", function(e) {
+    let currentScroll = window.pageYOffset
+    if (currentScroll >= 46) {
+        header.style.backgroundColor = 'rgba(37, 43, 66, .9)'
+    } else {
+        header.style.backgroundColor = 'transparent'
+    }
+    console.log('this is our event target', window.pageYOffset, currentScroll)
+})
+
+
+
+
+
+console.log('this is main.js !!!!', header)
 
